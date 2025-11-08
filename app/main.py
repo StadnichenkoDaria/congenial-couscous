4b08b0797b7cdb43e7b7fb8e7b8a705982ffcaa8
StadnichenkoDaria <brodiagad@gmail.com>
2025-11-08 12:55:09 +0300

    run docker (lesson 4)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import dotenv
 
 dotenv.load_dotenv()
@@ -5,10 +7,17 @@ dotenv.load_dotenv()
 import uvicorn
 from fastapi_pagination import add_pagination
 from fastapi import FastAPI
-from routers import status, users, root, login
+from app.routers import status, users, root, login
 from app.database.engine import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 app.include_router(root.router)
@@ -16,5 +25,4 @@ app.include_router(login.router)
 add_pagination(app)
 
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run(app, host="0.0.0.0", port=8000)
